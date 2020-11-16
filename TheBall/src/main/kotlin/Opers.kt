d50e6ef391a4740bb1cf04a6348f66ef0ce013d4
Pedro Pereira <palex@cc.isel.ipl.pt>
2020-11-16 12:03:59 +0000

    Aula 16 Novembro - Versão 3 Opers.kt

--- TheBall/src/main/kotlin/Opers.kt
+++ TheBall/src/main/kotlin/Opers.kt
@@ -11,20 +11,19 @@ fun readInt(quest:String) :Int {
     else readLine()!!.trim().toInt()
 }
 
-fun doOperation(operator:Char) {
+fun doOperation(operator:Char, fx : (Int,Int)->Int ) {
     val a = readInt("A")
     val b = readInt("B")
-    val res = when (operator) {
-        '+' -> a + b
-        '-' -> a - b
-        else -> a * b // smell code
-    }
+    val res = fx(a,b)
     println("$a $operator $b = $res")
 }
 
+fun add(a:Int, b:Int) :Int { return a + b }
+fun sub(a:Int, b:Int) :Int = a - b
+fun mult(a:Int, b:Int) = a * b
 
 fun main() {
-    doOperation('+')
-    doOperation('-')
-    doOperation('x')
+    doOperation('+',::add)
+    doOperation('-',::sub)
+    doOperation('x',::mult)
 }
